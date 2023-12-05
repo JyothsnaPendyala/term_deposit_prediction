f918867b01040b9c27d1c3b2bb0dd2609bfe72fc
praveencloudangles <112871831+praveencloudangles@users.noreply.github.com>
2023-12-05 16:45:33 +0530

    Update datavisualization.py

--- datavisualization.py
+++ datavisualization.py
@@ -44,8 +44,8 @@ def visualise_data():
         # fig.show()
         fig.write_image(f"{i}_hist.jpg")
         # a.append(fig)
-    columns_to_drop = ['y']
-    df = dataset.drop(columns=columns_to_drop, axis=1, inplace=True)
+    # columns_to_drop = ['y']
+    df = dataset.drop('y', axis=1, inplace=True)
     print("DFGHJKLPOIUYTR-------------",df)
     y=df.corr().columns.tolist()
     z=df.corr().values.tolist()
