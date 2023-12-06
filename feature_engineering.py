a3a2c23d1763645b0af3a85c92b5e24dc02a0611
praveencloudangles <112871831+praveencloudangles@users.noreply.github.com>
2023-12-06 18:57:47 +0530

    Update feature_engineering.py

--- feature_engineering.py
+++ feature_engineering.py
@@ -23,22 +23,22 @@ def feature_engineering():
         df2.drop(col, axis=1, inplace=True)
     
     #defaut features does not play imp role
-    df2.groupby(['y','default']).size()
+    df2.groupby(['y_new','default']).size()
     df2.drop(['default'],axis=1, inplace=True)
-    df2.groupby(['y','pdays']).size()
+    df2.groupby(['y_new','pdays']).size()
     df2.drop(['pdays'],axis=1, inplace=True)
     # remove outliers in feature age...
     df2.groupby('age',sort=True)['age'].count()
     # remove outliers in feature balance...
-    df2.groupby(['y','balance'],sort=True)['balance'].count()
+    df2.groupby(['y_new','balance'],sort=True)['balance'].count()
     # these outlier should not be remove as balance goes high, client show interest on deposit
     # remove outliers in feature campaign...
-    df2.groupby(['y','campaign'],sort=True)['campaign'].count()
+    df2.groupby(['y_new','campaign'],sort=True)['campaign'].count()
     df3 = df2[df2['campaign'] < 40]
-    df3.groupby(['y','campaign'],sort=True)['campaign'].count()
-    df3.groupby(['y','previous'],sort=True)['previous'].count()
+    df3.groupby(['y_new','campaign'],sort=True)['campaign'].count()
+    df3.groupby(['y_new','previous'],sort=True)['previous'].count()
     df4 = df3[df3['previous'] < 50]
-    df4.groupby(['y','previous'],sort=True)['previous'].count()
+    df4.groupby(['y_new','previous'],sort=True)['previous'].count()
     # cat_columns = ['job', 'marital', 'education', 'contact', 'month', 'poutcome']
     # for col in  cat_columns:
     #     df4 = pd.concat([df4.drop(col, axis=1),pd.get_dummies(df4[col], prefix=col, prefix_sep='_',drop_first=True, dummy_na=False)], axis=1)
